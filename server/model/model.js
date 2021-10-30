b63155988fa68ed1cedc4f411ffee1e5b236eeb4
Ravi-nanda-bala <ruderavi1998@gmail.com>
2021-10-30 21:31:03 +0530

    initial commit

---
+++ server/model/model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: true,
+    },
+    email : {
+        type: String,
+        required: true,
+    },
+    category : {
+        type: String,
+        required: true,
+    },
+    quantity : {
+        type: String,
+        required: true,
+    },
+    gender : String,
+    status : String
+})
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
